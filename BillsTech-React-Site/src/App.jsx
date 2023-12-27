e1e62e02723fda0829360c55b104efd0b3a85fa7
asadkamal07 <asadkamal531@gmail.com>
2023-12-27 06:55:48 +0500

    first commit

---
+++ BillsTech-React-Site/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { airbnb, amazon, calendar } from './assets/images'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ BillsTech-React-Site/src/assets/images/index.js
@@ -0,0 +1,25 @@
+import airbnb from './airbnb.svg'
+import amazon from './amazon.svg'
+import calendar from './calendar.svg'
+import facebook from './facebook.svg'
+import congrat from './congrat.svg'
+import google from './google.svg'
+import grab from './grab.svg'
+import netflix from './netflix.svg'
+import uxclass from './ux-class.svg'
+import girl from './girl.png'
+
+
+
+export {
+    airbnb,
+    amazon,
+    calendar,
+    facebook,
+    congrat,
+    google,
+    grab,
+    netflix,
+    uxclass,
+    girl
+}
