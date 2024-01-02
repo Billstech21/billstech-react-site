5eefe79457870b9f8b83f8a5e2a55b46faf313a0
Asad Kamal <asadkamal531@gmail.com>
2024-01-02 06:30:53 +0500

    update

--- BillsTech-React-Site/src/components/Banner.jsx
+++ BillsTech-React-Site/src/components/Banner.jsx
@@ -35,14 +35,6 @@ const Banner = () => {
         {/* <!--Right Col--> */}
         <div className="w-full lg:pt-16 lg:w-6/12 lg:-mt-10 relative" id="girl">
             <img data-aos="fade-up" data-aos-once="true" className="w-8/12 md:w-[85%] mx-auto 2xl:-mb-0" src={bannerImage} />
-            {/* <!-- calendar --> */}   
-            <div data-aos="fade-up" data-aos-delay="300" data-aos-once="true" className="absolute top-20 -left-6 sm:top-32 sm:left-10 md:top-40 md:left-16 lg:-left-0 lg:top-52 floating-4">
-                <img className="bg-white bg-opacity-80 rounded-lg h-12 sm:h-16" src={calendar}/>
-            </div>
-            {/* <!-- red --> */}
-            <div data-aos="fade-up" data-aos-delay="400" data-aos-once="true" className="absolute top-20 right-10 sm:right-24 sm:top-28 md:top-36 md:right-32 lg:top-32 lg:right-16 floating">
-                <svg className="h-16 sm:h-24" viewBox="0 0 149 149" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_d)"><rect x="40" y="32" width="69" height="69" rx="14" fill="#F3627C"/></g><rect x="51.35" y="44.075" width="47.3" height="44.85" rx="8" fill="white"/><path d="M74.5 54.425V78.575" stroke="#F25471" strokeWidth="4" strokeLinecap="round"/><path d="M65.875 58.7375L65.875 78.575" stroke="#F25471" stroke-width="4" stroke-linecap="round"/><path d="M83.125 63.9125V78.575" stroke="#F25471" stroke-width="4" stroke-linecap="round"/><defs><filter id="filter0_d" x="0" y="0" width="149" height="149" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood floodOpacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="8"/><feGaussianBlur stdDeviation="20"/><feColorMatrix type="matrix" values="0 0 0 0 0.825 0 0 0 0 0.300438 0 0 0 0 0.396718 0 0 0 0.26 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>
-            </div>
             {/* <!-- ux className --> */}
             <div data-aos="fade-up" data-aos-delay="500" data-aos-once="true" className="absolute bottom-14 -left-4 sm:left-2 sm:bottom-20 lg:bottom-24 lg:-left-4 floating">
                 <img className="bg-white bg-opacity-80 rounded-lg h-20 sm:h-28" src={BannerFloatImg2} alt=""/>
